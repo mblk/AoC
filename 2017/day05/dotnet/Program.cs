3d746cca7f540c1288f7de6e74ea5b81b53e7792
mblk <marcelblock@gmail.com>
2023-07-27 15:56:58 +0200

    AoC 2017 day 05 in dotnet.

---
+++ 2017/day05/dotnet/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace day05;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var jumplist1 = ParseJumpList("example");
+        var jumplist2 = ParseJumpList("input");
+        
+        Console.WriteLine($"Part1 example: {ExecuteJumpList(jumplist1)}");
+        Console.WriteLine($"Part1        : {ExecuteJumpList(jumplist2)}");
+        Console.WriteLine($"Part2 example: {ExecuteJumpList(jumplist1, true)}");
+        Console.WriteLine($"Part2        : {ExecuteJumpList(jumplist2, true)}");
+    }
+
+    private static int[] ParseJumpList(string fileName)
+    {
+        return File.ReadAllLines($"../../../../{fileName}")
+            .Select(Int32.Parse)
+            .ToArray();
+    }
+
+    private static int ExecuteJumpList(IEnumerable<int> jumplist, bool enhanced = false)
+    {
+        var jumps = jumplist.ToArray();
+        var position = 0;
+        var steps = 0;
+
+        while (position >= 0 && position < jumps.Length)
+        {
+            var offset = jumps[position];
+
+            if (enhanced && offset >= 3)
+                jumps[position]--;
+            else
+                jumps[position]++;
+
+            position += offset;
+            steps++;
+        }
+
+        return steps;
+    }
+}
